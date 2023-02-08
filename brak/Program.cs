4f967552a5ed4bee572e2094a36edd78050495b3
user0ren <88614567+user0ren@users.noreply.github.com>
2023-02-08 16:06:49 +0300

    Add files via upload

---
+++ brak/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Libr;
+
+
+namespace brak
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while(true)
+            {
+                int productType = 3;
+                int materialType = 1;
+                int count = 15;
+                float width = 20;
+                float length = 45;
+                int num = Calculation.GetQuantityForProduct(productType, materialType, count, width, length);
+                Console.WriteLine(num);
+                Console.ReadLine();
+            }
+        }
+    }
+}
